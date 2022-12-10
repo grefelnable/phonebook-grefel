cd9d9f2e519d8719b3c48197a9c9d70eab9fa05b
Grefel Nable <felnable@gmail.com>
2022-12-10 01:48:09 -0500

    Modify file by adding value and onchange

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,25 +1,34 @@
 import React from "react";
 
-const AddContact = ({ addPerson }) => {
+const AddContact = ({
+  addPerson,
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+}) => {
   return (
     <>
       <input type="checkbox" id="add-contact" className="modal-toggle" />
       <div className="modal">
         <form onSubmit={addPerson} className="modal-box">
-          <h3 className="font-bold text-lg mb-4">Add Contact</h3>
+          <h3 className="font-bold text-lg mb-8">Add Contact</h3>
           <input
             value={newName}
+            onChange={handleNameChange}
             type="text"
             placeholder="Name"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full max-w-xs mb-4"
           />
           <input
+            value={newNumber}
+            onChange={handleNumberChange}
             type="text"
             placeholder="Number"
             className="input input-bordered w-full max-w-xs"
           />
           <button type="submit" className="modal-action">
-            <label htmlFor="add-contact" className="btn btn-secondary">
+            <label htmlFor="add-contact" className="btn btn-secondary btn-sm">
               add
             </label>
           </button>
