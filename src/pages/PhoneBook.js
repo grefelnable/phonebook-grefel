4a3eaeddb0beb1f9686e3ba472d684dd5b675c95
Grefel Nable <felnable@gmail.com>
2022-12-10 01:48:56 -0500

    Add functions for newName and newNumber

--- src/pages/PhoneBook.js
+++ src/pages/PhoneBook.js
@@ -5,10 +5,20 @@ import Menu from "../components/Menu";
 const TheApp = () => {
   const [persons, setPersons] = useState([{ name: "Grefel Nable" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     console.log("submitted");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   };
   return (
     <main>
@@ -18,7 +28,13 @@ const TheApp = () => {
         {/* mid menu */}
         <div>mid menu</div>
       </div>
-      <AddContact addPerson={addPerson} />
+      <AddContact
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
     </main>
   );
 };
