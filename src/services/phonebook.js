5037785531d76987a05d6f292e0a907a9ec22756
Grefel Nable <felnable@gmail.com>
2022-12-12 04:04:08 -0500

    Add update and get specific contact on phonebook services

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -24,8 +24,24 @@ const deleteContact = (id) => {
   const contactDoc = doc(db, "phonebook", id);
   return deleteDoc(contactDoc);
 };
+
+// Update Contact
+const updateContact = (id, updatedContact) => {
+  // variable to check if the particular contact exists.
+  const contactDoc = doc(db, "phonebook", id);
+  return updateDoc(contactDoc, updatedContact);
+};
+
+// Get specific contact
+const getContact = (id) => {
+  const contactDoc = doc(db, "phonebook", id);
+  return getDoc(contactDoc);
+};
+
 export default {
   getAllContacts,
   addContact,
   deleteContact,
+  updateContact,
+  getContact,
 };
