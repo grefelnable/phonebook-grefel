b3f08dc63b7b0c3da94036a0557d282120971d49
Grefel Nable <felnable@gmail.com>
2022-12-15 00:51:28 -0500

    Add a link to the app's repository

--- src/pages/Repo.js
+++ src/pages/Repo.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Repo = () => {
-  return <div>Repo</div>;
+  return (
+    <div>
+      <h2 className="mb-8">Repository link</h2>
+      <a
+        className="link link-secondary text-lg"
+        rel="noopener"
+        target="_blank"
+        href="https://github.com/grefelnable/phonebook-grefel"
+      >
+        Phone Book App by Grefel
+      </a>
+    </div>
+  );
 };
 
 export default Repo;
