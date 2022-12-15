b568951dccd958672e64c5764dece35939d288c4
Grefel Nable <felnable@gmail.com>
2022-12-15 00:52:27 -0500

    Remove About page since this project is a link from portfolio

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import PhoneBook from "./pages/PhoneBook";
 import Code from "./pages/Code";
 import Repo from "./pages/Repo";
-import About from "./pages/About";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 
@@ -18,7 +17,6 @@ function App() {
           <Route path="phonebook" element={<PhoneBook />} />
           <Route path="code" element={<Code />} />
           <Route path="repo" element={<Repo />} />
-          <Route path="about" element={<About />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
